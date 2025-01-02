7a69b64a8716884c1139caf1c019794ed8f4f1c2
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-01-02 12:41:24 -0300

    Corrigindo.

--- scripts/whatsong.sh
+++ scripts/whatsong.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Verifica qual player está tocando e retorna o nome da música
-playerctl metadata title || echo "No song playing"
+playerctl metadata title || echo "Nenhuma música tocando."
 
