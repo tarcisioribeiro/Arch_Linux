304ba0c148cf8a04e26d5d8a5b22c620a5d05ef5
Tarcísio Ribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-09-10 22:20:25 -0300

    Corrigindo a aplicação.

--- scripts/network_config_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Wrapper para executar network_config.sh com sudo no autostart
-# Este script deve ter permissão NOPASSWD no sudoers
-
-sudo /home/tarcisio/Development/Arch_Linux/scripts/network_config.sh
